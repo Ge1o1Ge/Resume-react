feee73c8f2f57d77be4a7131cb48b7cea72f183b
Georg II <trajtv@gmail.com>
2023-10-10 20:19:34 +0200

    feat :about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import React from "react";
+import { motion } from "framer-motion";
+import { Tilt } from "react-tilt";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return <p>{title}</p>;
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+      <motion.p className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+        I&apos;m a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I&apos;m a quick learner and collaborate closely with clients
+        to create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let&apos;s work together to bring your ideas to
+        life!
+      </motion.p>
 
-export default About
+      <div className="mt20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,29 +1,61 @@
 /* eslint-disable react/no-unknown-property */
 
 import { Suspense, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, events } from "@react-three/fiber";
 import { useGLTF, Preload, OrbitControls } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1} />
-      <primitive object={computer.scene} />
+      <hemisphereLight intensity={0.3} groundColor="black" />
+      <pointLight intensity={0.85} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 };
 
 export const ComputersCanvas = () => {
+  const [isMobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    setMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       frameloop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
@@ -31,7 +63,7 @@ export const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
@@ -39,4 +71,4 @@ export const ComputersCanvas = () => {
   );
 };
 
-export default Computers;
+export default ComputersCanvas;
